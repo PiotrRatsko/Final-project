72673064af4d2b3fc156caa264015ea24ca031d5
Zitrasa <zitrasa@yandex.ru>
2021-04-04 11:10:32 +0300

    commit

--- InternetShop/InternetShop/Controllers/ProductsController.cs
+++ InternetShop/InternetShop/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InternetShop.Service;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,14 @@ namespace InternetShop.Controllers
 {
     public class ProductsController : Controller
     {
+        readonly ProductService _service;
+        public ProductsController(ProductService service)
+        {
+            _service = service;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_service);
         }
     }
 }
