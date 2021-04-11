98ab85ed316b27297a3130a5aa011874dff3fdb0
Zitrasa <zitrasa@yandex.ru>
2021-04-11 11:22:59 +0300

    commit

--- InternetShop/UnitTestApp/IndexViewBagMessage.cs
+++ InternetShop/UnitTestApp/IndexViewBagMessage.cs
@@ -3,29 +3,51 @@ using Microsoft.AspNetCore.Mvc;
 using InternetShop.Controllers;
 using InternetShop.Domain;
 using InternetShop.Domain.Repositories;
+using Moq;
+using System.Collections.Generic;
+using System;
+using InternetShop.Domain.Entities;
 
 namespace UnitTestApp
 {
-    public class ProductDetailControllerTests
+    public class HomeController_Tests
     {
         [SetUp]
         public void Setup()
         {
         }
 
-        //[Test]
-        //public void IndexViewBagMessageTest()
-        //{
-        //    // Arrange
-        //    IStoreRepository repo = new StoreRepository();
-        //    IStoreRepository repo = new DataManager(repo);
-        //    ProductDetailController controller = new ProductDetailController(dataManager);
+        [Test]
+        public void HomeController_Index_Tests()
+        {
+            // Arrange
+            var mockProduct = new Mock<IProductRepository>();
+            var mockUser = new Mock<IUserRepository>();
+            mockProduct.Setup(repo => repo.GetFilteredProducts(null, null, null)).Returns(GetFilteredProducts_Test());
+            mockUser.Setup(repo => repo.GetUserByEmail("User")).Returns(GetUserByEmail_Test());
+            var controller = new HomeController(mockProduct.Object, mockUser.Object);
 
-        //    // Act
-        //    ViewResult result = controller.Index(new System.Guid("ec516a6f-c93b-4a74-91ae-070f46076665")) as ViewResult;
+            // Act
+            var result = controller.Index();
+            ViewResult result2 = controller.Index() as ViewResult;
 
-        //    // Assert
-        //    Assert.AreEqual("Hello world!", result?.ViewData["Message"]);
-        //}
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            //var model = Assert.IsAssignableFrom<IEnumerable<User>>(viewResult.Model);
+            //Assert.Equal(GetTestUsers().Count, model.Count());
+        }
+        private List<Product> GetFilteredProducts_Test()
+        {
+            return new List<Product>()
+            {
+                new Product() { Name = "TestName1", Brand = "TestBrand1", Category = "TestCategory", Description = "TestDescription1", Id = new Guid("aa1a1111-a1a1-11a1-1111-1a1111aa1111"), Picter = "TestPicter1", Price = 1 },
+                new Product() { Name = "TestName2", Brand = "TestBrand2", Category = "TestCategory", Description = "TestDescription2", Id = new Guid("aa1a1111-a1a1-11a1-1111-1a1111aa1112"), Picter = "TestPicter2", Price = 2 }
+            };
+        }
+
+        private User GetUserByEmail_Test()
+        {
+            return new User();
+        }
     }
 }
